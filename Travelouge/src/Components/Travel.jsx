137228816ba3603189e9be6ef1eca272cd656e18
eiwaz123 <pandrewroi@gmail.com>
2024-08-14 21:30:53 +0800

    Revamp design

--- Travelouge/src/Components/Travel.jsx
+++ Travelouge/src/Components/Travel.jsx
@@ -5,24 +5,24 @@ const Travel = (props) => {
   return (
     <section className="overflow-hidden">
       <div data-aos="zoom-in-down">
-      <div className=" flex py-3 px-[2rem] md:px-[4rem] xl:px-[5rem] ">
-        <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-2 xl:grid-cols-2 gap-5">
+      <div className=" flex  px-[2rem] md:px-[4rem] xl:px-[5rem] ">
+        <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-2 xl:grid-cols-2 gap-2">
           <div className="  flex ">
             <div className=" ">
-              <img className="w-[60rem] py-10 rounded-full   " src={`${props.img}`} alt="Picture" />
+              <img className="w-[34rem] h-[35rem] py-10 rounded-full   " src={`${props.img}`} alt="Picture" />
             </div>
           </div>
-          <div className="  font-Gupter  xl:py-[6rem] lg:py-[5rem] md:py-[3rem] ">
+          <div className="  font-Gupter font-bold xl:py-[6rem] lg:py-[5rem] md:py-[3rem] ">
             <div className="  flex   items-center text-3xl py-2">
               <span>
                 <FaLocationDot className="text-red-400" />
               </span>
-              <span>{`${props.Location}`}</span>
+             <a href={`${props.Address}`} > <span>{`${props.Location}`}</span></a>
             </div>
             <div className=" flex flex-col justify-evenly  py-2  ">
               <h1 className="text-5xl py-2">{props.title}</h1>
-              <h1>{props.Description}</h1>
-              <h1 className="py-2">Date Traveled: {props.DateTravel}</h1>
+              <h1 className="text-2xl">{props.Description}</h1>
+              <h1 className=" text-2xl py-2">Date Traveled: {props.DateTravel}</h1>
             </div>
           </div>
         </div>
--- Travelouge/src/Components/data.jsx
+++ Travelouge/src/Components/data.jsx
@@ -10,7 +10,8 @@ export const datas=[
     Description:
       "A beautifull resort with lots of activities you can do not only they have vaierty of pool you can  also do some rock climbing and you can zimpline which gives a thrill for their visitors and the meal they serve is satisfactory and their hotel room is full satisfactory with soft beds bathroom with neccesities",
     DateTravel:2020,
-    Location:"Mataas na kahoy Batangas"
+    Location:"Mataas na kahoy Batangas",
+    Address:"https://www.google.com/maps/place/Shercon+Resort/@13.9944767,121.0966814,742m/data=!3m1!1e3!4m9!3m8!1s0x33bd6d94d71784a1:0x8a4828d7124888d6!5m2!4m1!1i2!8m2!3d13.9944767!4d121.0992563!16s%2Fg%2F11b6cb3spf?entry=ttu"
     },
     {
         id: 2,
@@ -19,7 +20,8 @@ export const datas=[
         Description:
           "A city built on mountain that has an interesting an history in which noast a large tourist among the country their market has wide variety of options to choose whether clothing food or other souviner for your friends or family  ",
         DateTravel:2024,
-        Location:"Baguio City"
+        Location:"Baguio City",
+        Address:"https://www.google.com/maps/place/Baguio,+Benguet/@16.3995536,120.5475751,11738m/data=!3m2!1e3!4b1!4m6!3m5!1s0x3391a16879def13f:0x8edef534be3a75c0!8m2!3d16.4023332!4d120.5960071!16zL20vMDE1bDFf?entry=ttu"
         },
         {
           id: 3,
@@ -28,6 +30,7 @@ export const datas=[
           Description:
             "Bonficaio Global Center also known as BGC is one of the most tourist spot for foreigner because it can offer different kinds of attractions for the different people all over the world",
           DateTravel:2024,
-          Location:"Makati City"
+          Location:"Makati City",
+          Address:"https://www.google.com/maps/place/Bonifacio+global+city/@14.5491212,121.0183369,1480m/data=!3m1!1e3!4m10!1m2!2m1!1sbonifacio+global+center!3m6!1s0x3397c90b1136a865:0x12940a608b81ef79!8m2!3d14.5507724!4d121.0440217!15sChdib25pZmFjaW8gZ2xvYmFsIGNlbnRlcpIBE2NvbmRvbWluaXVtX2NvbXBsZXjgAQA!16s%2Fg%2F11vkhzz34_?entry=ttu"
         },
 ];
