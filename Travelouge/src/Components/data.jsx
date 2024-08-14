a84f8fe5627a5e268b8d8da66916cd1fbe2690d3
eiwaz123 <pandrewroi@gmail.com>
2024-08-14 16:49:30 +0800

    fix image

--- Travelouge/src/Components/data.jsx
+++ Travelouge/src/Components/data.jsx
@@ -1,7 +1,7 @@
 export const datas=[
   {
     id: 1,
-    img: "./src/assets/Shercon.jpg",
+    img: "./public/Shercon.jpg",
     title: "Shercon Resort",
     Description:
       "A beautifull resort with lots of activities you can do not only they have vaierty of pool you can  also do some rock climbing and you can zimpline which gives a thrill for their visitors and the meal they serve is satisfactory and their hotel room is full satisfactory with soft beds bathroom with neccesities",
@@ -10,7 +10,7 @@ export const datas=[
     },
     {
         id: 2,
-        img: "./src/assets/baguio.jpg",
+        img: "./public/baguio.jpg",
         title: "Bagiou City",
         Description:
           "A city built on mountain that has an interesting an history in which noast a large tourist among the country their market has wide variety of options to choose whether clothing food or other souviner for your friends or family  ",
@@ -19,7 +19,7 @@ export const datas=[
         },
         {
           id: 3,
-          img: "./src/assets/BGC.jpg",
+          img: "./public/BGC.jpg",
           title: "Bonifacio Global Center City",
           Description:
             "Bonficaio Global Center also known as BGC is one of the most tourist spot for foreigner because it can offer different kinds of attractions for the different people all over the world",
