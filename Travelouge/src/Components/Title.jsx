a2c5b4b30e2c5279206ff2fedf4d8e67f015fc80
eiwaz123 <pandrewroi@gmail.com>
2024-08-14 20:52:35 +0800

    with animations

--- Travelouge/src/Components/Title.jsx
+++ Travelouge/src/Components/Title.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-
- const Title = () => {
-    return(
-        <div className="bg-red-300 flex justify-center items-center">
-            <h1 className=" text-white  font-bold font-Gupter text-3xl p-5">Travelouge </h1>
-        </div>
-    )
-}
-export default Title
+const Title = () => {
+  return (
+    <div data-aos="flip-left">
+      <nav className="bg-red-300   rounded-b-full">
+        <h1 className=" flex justify-center items-center text-white  font-bold font-Gupter text-3xl p-5">
+          Travelouge
+        </h1>
+      </nav>
+    </div>
+  );
+};
+export default Title;
--- Travelouge/src/Components/Travel.jsx
+++ Travelouge/src/Components/Travel.jsx
@@ -3,32 +3,32 @@ import "./data";
 import { FaLocationDot } from "react-icons/fa6";
 const Travel = (props) => {
   return (
-    <div className=" ">
-      <div className="  grid grid-cols-2 px-[15.5rem] ">
-        <div className=" flex justify-center items-center ">
-          <div className="flex justify-center items-center p-10">
-            <img className="w-96 h-30 rounded-md" src={`${props.img}`} alt="Picture" />
+    <section className="overflow-hidden">
+      <div data-aos="zoom-in-down">
+      <div className=" flex py-3 px-[2rem] md:px-[4rem] xl:px-[5rem] ">
+        <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-2 xl:grid-cols-2 gap-5">
+          <div className="  flex ">
+            <div className=" ">
+              <img className="w-[60rem] py-10 rounded-full   " src={`${props.img}`} alt="Picture" />
+            </div>
           </div>
-        </div>
-        <div className="  font-Gupter px-[5rem] py-[2rem] ">
-          <div className="flex justify-center items-center text-3xl py-2">
-            <span>
-              <FaLocationDot className="text-red-400" />
-            </span>
-            <span>{`${props.Location}`}</span>
-          </div>
-          <div className=" flex justify-center items-center py-2  text-4xl">
-            <h1>{props.title}</h1>
-          </div>
-          <div className="flex justify-center items-center py-2 ">
-            <h1>{props.Description}</h1>
-          </div>
-          <div>
-            <h1>Date Traveled: {props.DateTravel}</h1>
+          <div className="  font-Gupter  xl:py-[6rem] lg:py-[5rem] md:py-[3rem] ">
+            <div className="  flex   items-center text-3xl py-2">
+              <span>
+                <FaLocationDot className="text-red-400" />
+              </span>
+              <span>{`${props.Location}`}</span>
+            </div>
+            <div className=" flex flex-col justify-evenly  py-2  ">
+              <h1 className="text-5xl py-2">{props.title}</h1>
+              <h1>{props.Description}</h1>
+              <h1 className="py-2">Date Traveled: {props.DateTravel}</h1>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      </div>
+    </section>
   );
 };
 export default Travel;
